f732fd597d09553b1acd513cf18462b6fed6bc06
keisuke kashiwagi <mediateeee@outlook.com>
2025-08-15 19:21:08 +0800

    777

--- device.mk
+++ device.mk
@@ -16,8 +16,8 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # Cpoy prebuilt stuffs
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/prebuilt/dtb.img:out/target/product/rk3566_s/dtb.img \
-	$(DEVICE_PATH)/prebuilt/dtbo.img:out/target/product/rk3566_s/dtbo.img \
-	$(DEVICE_PATH)/prebuilt/kernel:out/target/product/rk3566_s/kernel
-
+    $(LOCAL_PATH)/prebuilt/dtb.img:out/target/product/rk3566_s/dtb.img \
+	$(LOCAL_PATH)/prebuilt/kernel:out/target/product/rk3566_s/kernel
+	
+#	$(DEVICE_PATH)/prebuilt/dtbo.img:out/target/product/rk3566_s/dtbo.img \
 
